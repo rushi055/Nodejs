b1115f86653af202fff2b94f3927b1c0b264c363
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2025-01-15 15:00:29 +0530

    Api calls through Postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,34 @@ const express = require("express");
 
 const app = express();
 
+//get request
+app.get("/test", (req, res) => {
+  res.send("Testing GET request");
+});
+
+//works for both /ab and /abc
+app.get("/ab?c", (req, res) => {
+  res.send("About page with ?");
+});
+
+//works for /ab , /abc , /abbc , /abbbbbbbbbc and so on
+app.get("/ab+c", (req, res) => {
+  res.send("About page with +");
+});
+
+//works for /abcd , /abxcd , /abFOOcd , /ab123cd and so on
+app.get("/ab*cd", (req, res) => {
+  res.send("About page with *");
+});
+
+
+//post request
+app.post("/test", (req, res) => {
+  console.log("POST request received");
+  res.send("Testing POST request");
+});
+
+//use is used for all the request types
 app.use("/test", (req, res) => {
   res.send("/test request received");
 });
@@ -10,8 +38,9 @@ app.use((req, res) => {
   res.send(" request received");
 });
 
-app.listen(3000, () => {
+app .listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 //nodeman src/app.js
+// we can also add the script in package.json file to run the server using command npm run "name"
